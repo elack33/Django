bf0cde536c2ed094c3a5567b365c62d33f0d78c1
Dar Wright <elack33@gmail.com>
2015-08-31 17:43:01 -0700

    learning from last week

--- mysite/jobpost/tests.py
+++ mysite/jobpost/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from . import forms
+
+
+class TestCreateJobPostForm(TestCase):
+
+    def setUp(self):
+        self.data = {
+            'title': 'Big Green Wheeling Machine',
+            'job_description': 'Dodging shells, eating power ups and flying through turns!',
+            'Roles': 'RO',
+        }
+
+    def test_creates_roles_field_at_init(self):
+        form = forms.NewJobPost()
+        self.assertIsNotNone(form.fields.get('Roles'))
--- mysite/myaccounts/tests.py
+++ mysite/myaccounts/tests.py
@@ -1,5 +1,1 @@
-from django.test import TestCase
 
-from .import forms
-
-# Create your tests here.
